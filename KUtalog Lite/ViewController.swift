7d8af3ee330b21efb0fda27a616ac5cfe6247ee5
Hasan Can <aslan4044@gmail.com>
2020-01-19 14:50:54 +0300

    initial commit

---
+++ KUtalog Lite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KUtalog Lite
+//
+//  Created by HASAN CAN on 19.01.2020.
+//  Copyright © 2020 hasancanaslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
