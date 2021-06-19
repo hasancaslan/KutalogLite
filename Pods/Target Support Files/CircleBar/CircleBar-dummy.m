acb82ef23faf4f0ba6ac87e89025b1c75e90f5e5
Hasan Can Aslan <42111960+hasancaslan@users.noreply.github.com>
2021-06-19 10:28:22 +0300

    Delete Pods directory

--- Pods/Target Support Files/CircleBar/CircleBar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CircleBar : NSObject
-@end
-@implementation PodsDummy_CircleBar
-@end
--- Pods/Target Support Files/Pods-KUtalog/Pods-KUtalog-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KUtalog : NSObject
-@end
-@implementation PodsDummy_Pods_KUtalog
-@end
